11d68bac4ca7998ea4fcc5404d5d232b897043e7
Nicholas Colaprete <ncolaprete@gmail.com>
2015-09-05 22:07:30 -0400

    Created Cached Search Test
    Added initial build

---
+++ C#/CachedSearch/CachedSearch/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CachedSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.displayLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
+            this.mainSplitContainer.Panel1.SuspendLayout();
+            this.mainSplitContainer.Panel2.SuspendLayout();
+            this.mainSplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainSplitContainer
+            // 
+            this.mainSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainSplitContainer.IsSplitterFixed = true;
+            this.mainSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.mainSplitContainer.Name = "mainSplitContainer";
+            this.mainSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // mainSplitContainer.Panel1
+            // 
+            this.mainSplitContainer.Panel1.Controls.Add(this.searchBox);
+            // 
+            // mainSplitContainer.Panel2
+            // 
+            this.mainSplitContainer.Panel2.Controls.Add(this.displayLabel);
+            this.mainSplitContainer.Size = new System.Drawing.Size(270, 939);
+            this.mainSplitContainer.SplitterDistance = 27;
+            this.mainSplitContainer.TabIndex = 0;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new System.Drawing.Point(0, 0);
+            this.searchBox.MaxLength = 50;
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(270, 29);
+            this.searchBox.TabIndex = 0;
+            this.searchBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.AutoSize = true;
+            this.displayLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.displayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayLabel.Location = new System.Drawing.Point(0, 0);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(0, 20);
+            this.displayLabel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 939);
+            this.Controls.Add(this.mainSplitContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "W‌indow";
+            this.mainSplitContainer.Panel1.ResumeLayout(false);
+            this.mainSplitContainer.Panel1.PerformLayout();
+            this.mainSplitContainer.Panel2.ResumeLayout(false);
+            this.mainSplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).EndInit();
+            this.mainSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer mainSplitContainer;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Label displayLabel;
+    }
+}
+
---
+++ C#/CachedSearch/CachedSearch/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace CachedSearch
+{
+    public partial class Form1 : Form
+    {
+        CachedSearchManager searchManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            StreamReader sread = new StreamReader(assembly.GetManifestResourceStream("CachedSearch.Thesaurus.txt"));
+            string[] contents = sread.ReadToEnd().Split(new char[] {'\n'});
+            List<string> data = new List<string>(contents);
+            searchManager = new CachedSearchManager(data);
+        }
+
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+            searchBox.Text = searchBox.Text.ToLower();
+            string searchTerm = searchBox.Text.Trim();
+            displayLabel.Text = "";
+            if (searchTerm == "")
+                return;
+            List<string> searchResults = searchManager.Search(searchTerm);
+            for (int i=0;i<45;i++)
+            {
+                if (i >= searchResults.Count)
+                    break;
+                displayLabel.Text += searchResults[i] + "\n";
+            }
+        }
+    }
+}
+
+public class CachedSearchManager
+{
+    public List<string> Dataset;
+    public List<CacheItem> Cacheset;
+
+    public CachedSearchManager(List<string> dataset)
+    {
+        this.Dataset = dataset;
+        this.Cacheset = new List<CacheItem>();
+    }
+
+    private static List<string> Filter(string term, List<string> data)
+    {
+        List<string> finalList = new List<string>();
+        foreach (string item in data)
+        {
+            if (item.Contains(term))
+                finalList.Add(item);
+        }
+        return finalList;
+    }
+
+    private CacheItem GetCacheItemForTerm(string term)
+    {
+        int LongestCachedTerm = -1;
+        for (int i=0;i<Cacheset.Count;i++)
+        {
+            CacheItem item = Cacheset[i];
+            if (term == item.Term)
+                return item;
+            else if (term.StartsWith(item.Term) &&
+                (LongestCachedTerm == -1 || Cacheset[LongestCachedTerm].Term.Length < item.Term.Length))
+                LongestCachedTerm = i;
+        }
+        if (LongestCachedTerm == -1)
+            return null;
+        else
+            return Cacheset[LongestCachedTerm];
+    }
+
+    public List<string> Search(string term)
+    {
+        CacheItem fromCache = GetCacheItemForTerm(term);
+        List<string> localResults;
+        if (fromCache == null)
+            localResults = CachedSearchManager.Filter(term, Dataset);
+        else if (fromCache.Term == term)
+            return fromCache.Results;
+        else
+            localResults = CachedSearchManager.Filter(term, fromCache.Results);
+        Cacheset.Add(new CacheItem(term, localResults));
+        return localResults;
+    }
+
+    public class CacheItem
+    {
+        public string Term;
+        public List<string> Results;
+
+        public CacheItem(string term, List<string> results)
+        {
+            this.Term = term;
+            this.Results = results;
+        }
+    }
+}
